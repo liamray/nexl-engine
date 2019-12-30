42dd9981d7a763c663996df10c1516ec07ad3589
liamr <info@nexl-js.com>
2019-12-30 19:28:24 +0200

    - working on metadata parser

--- tests/test-md-parser.js
+++ tests/test-md-parser.js
@@ -1,5 +1,7 @@
 const nexlEngine = require('../nexl-engine/nexl-engine');
-const source = {filePath: 'nexl-sources/nexl-source1.js'};
+let source;
+source = {filePath: 'nexl-sources/nexl-source1.js'};
+source = {filePath: 'md-sources/md-src1.js'};
 
 const result = nexlEngine.parseMD(source);
 console.log(result);

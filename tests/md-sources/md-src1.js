0fa920e15c9216e44d0b2f6f436adbdae98c7238
liamray <liamraymisc@gmail.com>
2020-01-09 23:18:16 +0200

    - md collection

--- tests/md-sources/md-src1.js
+++ tests/md-sources/md-src1.js
@@ -1,25 +1,81 @@
-PEARL_BATCH = {'PRE-UT': ['pearlbatchut3'], APPDEV: ['pearlbatchut3'], USERTEST: ['pearlbatchut1'], YEST: ['pearlbatchyest1'], TRAIN: ['pearlbatchtrain1'], PROD: ['pearlbatchprod1']};
-1;
-"1";
-true;
-[];
-{};
-a = null;
-b = undefined;
-c = NaN;
-d = 1;
-e = "a";
-f = true;
-g = {a: 1, zzz: "100", yyy: {}};
-h = [];
-i = (aaa, bbb, ccc) => {};
+a1 = null;
+b1 = undefined;
+c1 = NaN;
+d1 = 1;
+e1 = "a";
+f1 = true;
+g1 = {a: 1, zzz: "100", yyy: {}};
+h1 = [];
 i1 = () => {
 };
-j = function (ggg, hhh, jjj) {};
-j1 = function () {
+j1 = (aaa, bbb, ccc) => {
 };
-k = Math;
-l = a;
+k1 = function () {
+};
+l1 = function (ggg, hhh, jjj) {
+};
+m1 = Math;
+n1 = a1;
+
+const a2 = null;
+const b2 = undefined;
+const c2 = NaN;
+const d2 = 1;
+const e2 = "a";
+const f2 = true;
+const g2 = {a: 1, zzz: "100", yyy: {}};
+const h2 = [];
+const i2 = () => {
+};
+const j2 = (aaa, bbb, ccc) => {
+};
+const k2 = function () {
+};
+const l2 = function (ggg, hhh, jjj) {
+};
+const m2 = Math;
+const n2 = a1;
+
+let a3 = null;
+let b3 = undefined;
+let c3 = NaN;
+let d3 = 1;
+let e3 = "a";
+let f3 = true;
+let g3 = {a: 1, zzz: "100", yyy: {}};
+let h3 = [];
+let i3 = () => {
+};
+let j3 = (aaa, bbb, ccc) => {
+};
+let k3 = function () {
+};
+let l3 = function (ggg, hhh, jjj) {
+};
+let m3 = Math;
+let n3 = a1;
+
+
+var a4 = null;
+var b4 = undefined;
+var c4 = NaN;
+var d4 = 1;
+var e4 = "a";
+var f4 = true;
+var g4 = {a: 1, zzz: "100", yyy: {}};
+var h4 = [];
+var i4 = () => {
+};
+var j4 = (aaa, bbb, ccc) => {
+};
+var k4 = function () {
+};
+var l4 = function (ggg, hhh, jjj) {
+};
+var m4 = Math;
+var n4 = a1;
+
+
 aaa.bbb.ccc.ddd = 100;
 this['CLIENT-ZONE'] = {
 	TEST: ['pearlappdev1'],
@@ -30,7 +86,19 @@ this['CLIENT-ZONE'] = {
 	PROD: ['pearlappczoneprod1', 'pearlappczoneprod2']
 };
 nexl.args = 1;
-function test2(xxx, yyy, zzz) {}
+
+function test2(xxx, yyy, zzz) {
+}
 
 function test3() {
+}
+
+PEARL_BATCH = {'PRE-UT': ['a1'], APPDEV: ['a2'], USERTEST: ['a3'], YEST: ['a4'], TRAIN: ['a5'], PROD: ['a6']};
+
+1;
+"1";
+'1';
+true;
+[];
+{
 }
